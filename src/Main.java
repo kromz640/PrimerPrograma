c1e1dd32c1519aa1fd7fcfed81ba756dac7e081f
Kronoz640 <rroquek@msn.com>
2024-04-07 15:24:22 -0600

    primer comit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hola mundo");
+        System.out.println("Bienvenido a Screen match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan=true;
+        double notaDeLaPelicula=8.2;
+        double media=(8.2+6.0+9.0)/3;
+        System.out.println(media);
+
 
         }
     }
