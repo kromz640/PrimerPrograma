c72dea2d58d76fa05bdccc4b808a1c603dfcc774
Rolando Roque Kuan <rroquek@msn.com>
2024-04-12 08:26:18 -0600

    primer comit

--- src/CuentaBanco.java
+++ src/CuentaBanco.java
@@ -1,7 +1,15 @@
-public class CuentaBanco
-{
-    public static void main(String[] args) {
-
+public class CuentaBanco {
+    public static String mensaje (int n)  {
+        String sNumVueltas = "";
+        for( int i=1; i < n; i++)  {
+            i++;
+            sNumVueltas += "\nOtra mas";
+            System.out.println("i =" + i + "\t");
+        } // for
+        return sNumVueltas;
     }
-}
 
+    public static void main(String[] args) {
+        System.out.println ("El resultado es:" + mensaje (6) );
+    }// main
+} // class
